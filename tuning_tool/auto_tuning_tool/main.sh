e6ca327f8f9a68a95442e5dccf8f38d0eb1f0462
hongjunchu <45329527+hongjunchu@users.noreply.github.com>
2019-10-09 12:48:31 +0800

    Update main.sh

--- tuning_tool/auto_tuning_tool/main.sh
+++ tuning_tool/auto_tuning_tool/main.sh
@@ -1,8 +1,8 @@
 python main.py \
     --proto /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/deploy.prototxt \
     --model /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/custom.caffemodel \
-    --calibration_proto "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_calibration_table.pb2" \  
-    --calibration_model "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_int8.caffemodel" \  
+    --calibration_proto "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_calibration_table.pb2" \
+    --calibration_model "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_int8.caffemodel" \
     --output_path /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ \
     --data_list /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ILSVRC2012_val/input.txt \
     --data_limit 20 \
