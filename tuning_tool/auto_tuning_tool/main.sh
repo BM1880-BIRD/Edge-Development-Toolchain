fa1904a969df35caf507ef74a5b66e12cd2e0f2d
hongjun.chu <hongjun.chu@bitmain.com>
2019-10-09 17:13:26 +0800

    use calibration tool image for tuning tool demo

--- tuning_tool/auto_tuning_tool/main.sh
+++ tuning_tool/auto_tuning_tool/main.sh
@@ -4,7 +4,7 @@ python main.py \
     --calibration_proto "/opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_calibration_table.pb2" \
     --calibration_model "/opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_int8.caffemodel" \
     --output_path /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ \
-    --data_list /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ILSVRC2012_val/input.txt \
+    --data_list /opt/Edge-Development-Toolchain/calibration_tool/Resnet50/ILSVRC2012_val/input.txt \
     --data_limit 20 \
     --image_params "image_params.json" \
     --ignore_layer_list 'data'  
