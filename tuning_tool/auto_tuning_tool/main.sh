9b15c09e888363df1b02f33cca08f65cee058595
hongjun.chu <hongjun.chu@bitmain.com>
2019-10-09 16:43:18 +0800

    update sample script path

--- tuning_tool/auto_tuning_tool/main.sh
+++ tuning_tool/auto_tuning_tool/main.sh
@@ -1,10 +1,10 @@
 python main.py \
-    --proto /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/deploy.prototxt \
-    --model /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/custom.caffemodel \
-    --calibration_proto "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_calibration_table.pb2" \
-    --calibration_model "/home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_int8.caffemodel" \
-    --output_path /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ \
-    --data_list /home/chjtest/compiler/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ILSVRC2012_val/input.txt \
+    --proto /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/deploy.prototxt \
+    --model /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/custom.caffemodel \
+    --calibration_proto "/opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_calibration_table.pb2" \
+    --calibration_model "/opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/bmnet_Resnet50_int8.caffemodel" \
+    --output_path /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ \
+    --data_list /opt/Edge-Development-Toolchain/tuning_tool/auto_tuning_tool/Resnet50/ILSVRC2012_val/input.txt \
     --data_limit 20 \
     --image_params "image_params.json" \
     --ignore_layer_list 'data'  
